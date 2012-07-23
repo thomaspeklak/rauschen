c6499cc477820d18222711fd6f1405cd33e9bcff
Thomas Peklak <thomas.peklak@gmail.com>
2012-07-23 23:15:05 +0200

    add 204 response when valid timings are presented

---
+++ frontend/routes/index.js
@@ -0,0 +1,7 @@
+module.exports = function(app){
+	app.post("/", function(req, res){
+		res.send(204);
+	});
+
+
+};
--- frontend/server.js
+++ frontend/server.js
@@ -2,8 +2,10 @@ var  express = require("express");
 
 app = express.createServer();
 
-console.log(__dirname + '/public');
-app.use(express.static(__dirname + '/public'));
+console.log(__dirname + "/public");
+app.use(express.static(__dirname + "/public"));
 app.use(express.bodyParser());
+
+require("./routes")(app);
 
 module.exports = app;
