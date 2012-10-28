3d19f0636e3b791e7fef9fef0f61d474a7f38796
Thomas Peklak <thomas.peklak@gmail.com>
2012-10-28 22:30:48 +0100

    add geoip enrichment

--- backend/lib/geoip.js
+++ backend/lib/geoip.js
@@ -3,7 +3,15 @@ var country     = new geoip.Country(process.env.GEOIP_COUNTRY_DAT);
 var country_v6  = new geoip.Country6(process.env.GEOIP_COUNTRY_DAT_V6);
 var city        = new geoip.City(process.env.GEOIP_CITY_DAT);
 var city_v6     = new geoip.City6(process.env.GEOIP_CITY_DAT_V6);
+var async       = require("async");
 
 module.exports = function(ip, cb){
-    cb({});
+    if(ip.indexOf(':') >= 0){
+        cb({});
+    } else {
+        async.parallel({
+            country : function(cb){ country.lookup(ip, cb); },
+            city    : function(cb){ city.lookup(ip, cb); }
+        }, cb);
+    }
 };
--- test/backend/geoip.spec.js
+++ test/backend/geoip.spec.js
@@ -1,12 +1,29 @@
-var geoipData = require(__dirname + '/../../backend/lib/geoip');
+var geoipData = require(__dirname + "/../../backend/lib/geoip");
 
-describe('GeoipData', function(){
-    it('should provide country for an IPv4', function(done){
-        geoipData('8.8.8.8', function(data){
-            data.countryCode.should.be('US');
-            data.countryCode3.should.be('USA');
-            data.countryName.should.be('United States');
-            data.continent.should.be('NA');
+describe("GeoipData", function(){
+    it("should provide country for an IPv4", function(done){
+        geoipData("8.8.8.8", function(err, data){
+            data.should.have.property('country');
+            var country = data.country;
+            country.should.have.property("country_code", "US");
+            country.should.have.property("country_code3", "USA");
+            country.should.have.property("country_name", "United States");
+            country.should.have.property("continent_code", "NA");
+            done();
+        });
+    });
+    it("should provide city for an IPv4", function(done){
+        geoipData("8.8.8.8", function(err, data){
+            data.should.have.property('city');
+            var city = data.city;
+            city.should.have.property("country_code3", "USA");
+            city.should.have.property("country_name", "United States");
+            city.should.have.property("continent_code", "NA");
+            city.should.have.property("region", "CA");
+            city.should.have.property("city", "Mountain View");
+            city.should.have.property("postal_code", "94043");
+            city.should.have.property("latitude", 37.4192008972168);
+            city.should.have.property("longitude", -122.05740356445312);
             done();
         });
     });
