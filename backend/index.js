3105ea5548f7eafd53f52bcc4a809d0d30529b6f
Thomas Peklak <thomas.peklak@gmail.com>
2012-10-29 16:58:25 +0100

    process data in parallel

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,8 @@ var net               = require("net");
 var Scuttlebutt       = require("scuttlebutt/model");
 var timing            = new Scuttlebutt();
 var socket            = "/tmp/rauschen.sock";
-var geoDataProvider   = require("./lib/geoip.js");
-var userAgentDetector = require("./lib/user-agent-detector.js");
+var processGeoData   = require("./lib/geoip.js");
+var processUserAgent= require("./lib/user-agent-detector.js");
 
 var timingStream = timing.createStream();
 
@@ -11,12 +11,20 @@ timingStream.pipe(net.connect(socket)).pipe(timingStream);
 
 timing.on("update", function(key){
     var data = timing.data;
-    geoDataProvider(data.remoteAddress, function(geoData){
-        data.geo = geoData;
-    });
-
-    userAgentDetector(data.userAgent, function(userAgentData){
-        data.userAgentData = userAgentData;
+    async.parallel({
+        geo: function(){
+            geoDataProvider(data.remoteAddress, function(geoData){
+                data.geo = geoData;
+            });
+        },
+        userAgent: function(){
+            userAgentDetector(data.userAgent, function(userAgentData){
+                data.userAgentData = userAgentData;
+            });
+        }
+    },
+    function(){
+        console.log('done');
     });
 });
 
