cd1afecaf26860475432b36b5b867dfa2d92dea4
Thomas Peklak <thomas.peklak@gmail.com>
2012-11-08 06:51:20 +0100

    remove async module from backend

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,20 @@
 var net                  = require("net");
-var async                = require("async");
 var Scuttlebutt          = require("scuttlebutt/model");
 var timing               = new Scuttlebutt();
 var socket               = "/tmp/rauschen.sock";
 var dataExtractionStream = require("./lib/data-extraction-stream.js");
 var dataEnrichStream     = require("./lib/data-enrich-stream.js");
+var persistenceStream    = require("./lib/persistence-stream.js").create();
+
 
 var timingStream = timing.createStream();
 
 timingStream.pipe(net.connect(socket)).pipe(timingStream);
 
-timingStream
-    .pipe(dataExtractionStream)
-    .pipe(dataEnrichStream)
-    .pipe(process.stdout);
+var enrichedStream = timingStream
+                        .pipe(dataExtractionStream)
+                        .pipe(dataEnrichStream);
+
+//enrichedStream.pipe(process.stdout);
+enrichedStream.pipe(persistenceStream);
 
