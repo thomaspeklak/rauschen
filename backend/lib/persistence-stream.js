c66fd56a3a4aa0a8cc504106f874ace485eaa69c
Thomas Peklak <thomas.peklak@gmail.com>
2012-11-12 21:49:04 +0100

    add persistence

--- backend/lib/persistence-stream.js
+++ backend/lib/persistence-stream.js
@@ -1,22 +1,43 @@
-var timing = require(__dirname + "/../../models/timing");
+//MongoDB
+var Db = require('mongodb').Db;
+var Connection = require('mongodb').Connection;
+var Server = require('mongodb').Server;
+
+var pauseStream = require('pause-stream')();
+pauseStream.pause();
+
 var Stream = require("stream");
-
-
 var ps = new Stream();
 ps.writable = true;
 
+pauseStream.pipe(ps);
+
 ps.write = function (buf) {
     var data = JSON.parse(buf);
+    //timings.insert(data);
 };
 
 ps.end = function (buf) {
-        if (arguments.length) this.write(buf);
-        this.destroy();
+    if (arguments.length) this.write(buf);
+    this.destroy();
 };
 
 ps.destroy = function(){
     this.writeable = false;
 };
 
-module.exports = ps;
+
+var db = new Db('rauschen', new Server("127.0.0.1", 27017, {auto_reconnect: true, poolSize: 40}), {safe:false, native_parser: true});
+var timings ;
+
+db.open(function(err, db){
+    if(err){
+        console.log(err);
+        process.exit();
+    }
+    timings = db.collection('timings');
+    pauseStream.resume();
+});
+
+module.exports = pauseStream;
 
