ab9fdf271e268b6c959305febc716ce3422c2142
Thomas Peklak <thomas.peklak@gmail.com>
2012-11-13 17:21:15 +0100

    reorganize enrich stream

--- backend/lib/data-enrich-stream.js
+++ backend/lib/data-enrich-stream.js
@@ -4,6 +4,33 @@ var geoip       = require("./geoip");
 var userAgent   = require("./user-agent-detector");
 var url         = require("./url-disector");
 
+var parallelEnrichers = {
+    geo: function(cb){
+        geoip(data.remoteAddress, function(err, geoData){
+            if(!err){ data.geo = geoData; }
+            cb();
+        });
+    },
+    userAgent: function(cb){
+        userAgent(data.userAgent, function(err, userAgentData){
+            if(!err){ data.userAgentData = userAgentData; }
+            cb();
+        });
+    },
+    referer: function(cb){
+        url(data.referer, function(err, urlData){
+            if(!err){ data.url = urlData; }
+            cb();
+        });
+    }
+};
+
+var processStream = function(data, stream){
+    async.parallel( parallelEnrichers, function(){
+        stream.queue(JSON.stringify(data));
+    });
+};
+
 module.exports = through(function write(data){
     if(typeof data !== 'string') return;
     var parsedData = JSON.parse(data);
@@ -11,35 +38,3 @@ module.exports = through(function write(data){
 }, function end(){
     this.queue();
 });
-
-var processStream = function(data, stream){
-    async.parallel({
-        geo: function(cb){
-            geoip(data.remoteAddress, function(err, geoData){
-                if(!err){
-                    data.geo = geoData;
-                }
-                cb();
-            });
-        },
-        ua: function(cb){
-            userAgent(data.userAgent, function(err, userAgentData){
-                if(!err){
-                    data.userAgentData = userAgentData;
-                }
-                cb();
-            });
-        },
-        ref: function(cb){
-            url(data.referer, function(err, urlData){
-                if(!err){
-                    data.url = urlData;
-                }
-                cb();
-            });
-        }
-    }, function(){
-        stream.queue(JSON.stringify(data));
-    });
-};
-
