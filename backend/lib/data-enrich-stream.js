2d81fc7cd1d13f59d94a45b122e395d8b69bb5e9
Thomas Peklak <thomas.peklak@gmail.com>
2012-11-14 23:58:36 +0100

    revert back without promises

--- backend/lib/data-enrich-stream.js
+++ backend/lib/data-enrich-stream.js
@@ -1,39 +1,30 @@
 var through     = require("through");
-var q           = require('q');
 var geoip       = require("./geoip");
 var userAgent   = require("./user-agent-detector");
 var url         = require("./url-disector");
 
-var defer = function(fn){
-    var deferred = q.defer();
-    fn(deferred);
-    return deferred;
-};
-
 var processStream = function(data, stream){
-    var dGeo = defer(function(deferred){
-        geoip(data.remoteAddress, function(err, result){
-            if(result){ data.geo = result;}
-            deferred.resolve();
-        });
+    var count = 0;
+    var passStream = function(){
+        count += 1;
+        if(count == 3){
+            stream.queue(JSON.stringify(data));
+        }
+    };
+
+    geoip(data.remoteAddress, function(err, result){
+        if(result){ data.geo = result;}
+        passStream();
     });
-    var dUserAgent = defer(function(deferred){
-        userAgent(data.userAgent, function(err, result){
-            if(result){ data.userAgent = result ;}
-            deferred.resolve();
-        });
+    userAgent(data.userAgent, function(err, result){
+        if(result){ data.userAgent = result ;}
+        passStream();
     });
-    var dReferer = defer(function(deferred){
-        url(data.referer, function(err, result){
-            if(result){ data.referer = result;}
-            deferred.resolve();
-        });
+    url(data.referer, function(err, result){
+        if(result){ data.referer = result;}
+        passStream();
     });
 
-    q.all([dGeo, dUserAgent, dReferer])
-        .then(function(){
-            stream.queue(JSON.stringify(data));
-        });
 };
 
 module.exports = through(function write(data){
