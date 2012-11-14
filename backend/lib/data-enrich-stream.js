2754d80f006a29fd6e162eae24533a076464153b
Thomas Peklak <thomas.peklak@gmail.com>
2012-11-15 00:43:14 +0100

    refactor into function into constructor function to prevent inconsistencies

--- backend/lib/data-enrich-stream.js
+++ backend/lib/data-enrich-stream.js
@@ -5,31 +5,32 @@ var url         = require("./url-disector");
 
 var createCallback = function(type){
     return function(err, result){
-        if(result){ data[type] = result; }
-        passStream();
+        if(result){ this.data[type] = result; }
+        this.passStream();
     };
 };
 
-var processStream = function(data, stream){
-    var count = 0;
+var StreamProcessor = function(data, stream){
+    this.count = 0;
     this.data = data;
-    this.passStream = function(type, result){
-        count += 1;
-        if(count == 3){
-            stream.queue(JSON.stringify(data));
-        }
-    };
+    this.stream = stream;
 
     geoip(data.remoteAddress , createCallback('geo').bind(this));
     userAgent(data.userAgent , createCallback('userAgent').bind(this));
     url(data.referer         , createCallback('referer').bind(this));
 
 };
+StreamProcessor.prototype.passStream = function(type, result){
+    this.count += 1;
+    if(this.count == 3){
+        this.stream.queue(JSON.stringify(this.data));
+    }
+};
 
 module.exports = through(function write(data){
     if(typeof data !== 'string') return;
     var parsedData = JSON.parse(data);
-    processStream(parsedData, this);
+    new StreamProcessor(parsedData, this);
 }, function end(){
     this.queue();
 });
