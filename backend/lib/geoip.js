470d9638f2d927cfbf4e4e096ff8f569b58dba34
Thomas Peklak <thomas.peklak@gmail.com>
2012-11-14 21:38:09 +0100

    simplify geoip lookup

--- backend/lib/geoip.js
+++ backend/lib/geoip.js
@@ -1,23 +1,15 @@
 var geoip       = require("geoip");
-var country_v4  = new geoip.Country(process.env.GEOIP_COUNTRY_DAT);
-var country_v6  = new geoip.Country6(process.env.GEOIP_COUNTRY_DAT_V6);
 var city_v4     = new geoip.City(process.env.GEOIP_CITY_DAT);
 var city_v6     = new geoip.City6(process.env.GEOIP_CITY_DAT_V6);
-var async       = require("async");
 
 module.exports = function(ip, cb){
     var city, country;
 
     if(ip.indexOf(":") >= 0){
         city = city_v6;
-        country = country_v6;
     } else {
         city = city_v4;
-        country = country_v4;
     }
 
-    async.parallel({
-        country : function(cb){ country.lookup(ip, cb); },
-        city    : function(cb){ city.lookup(ip, cb); }
-    }, cb);
+    city.lookup(ip, cb);
 };
--- test/backend/geoip.spec.js
+++ test/backend/geoip.spec.js
@@ -3,52 +3,44 @@ var geoipData = require(__dirname + "/../../backend/lib/geoip");
 describe("GeoipData", function(){
     it("should provide country for an IPv4", function(done){
         geoipData("8.8.8.8", function(err, data){
-            data.should.have.property("country");
-            var country = data.country;
-            country.should.have.property("country_code", "US");
-            country.should.have.property("country_code3", "USA");
-            country.should.have.property("country_name", "United States");
-            country.should.have.property("continent_code", "NA");
+            data.should.have.property("country_code", "US");
+            data.should.have.property("country_code3", "USA");
+            data.should.have.property("country_name", "United States");
+            data.should.have.property("continent_code", "NA");
             done();
         });
     });
     it("should provide city for an IPv4", function(done){
         geoipData("8.8.8.8", function(err, data){
-            data.should.have.property("city");
-            var city = data.city;
-            city.should.have.property("country_code3", "USA");
-            city.should.have.property("country_name", "United States");
-            city.should.have.property("continent_code", "NA");
-            city.should.have.property("region", "CA");
-            city.should.have.property("city", "Mountain View");
-            city.should.have.property("postal_code", "94043");
-            city.should.have.property("latitude", 37.4192008972168);
-            city.should.have.property("longitude", -122.05740356445312);
+            data.should.have.property("country_code3", "USA");
+            data.should.have.property("country_name", "United States");
+            data.should.have.property("continent_code", "NA");
+            data.should.have.property("region", "CA");
+            data.should.have.property("city", "Mountain View");
+            data.should.have.property("postal_code", "94043");
+            data.should.have.property("latitude", 37.4192008972168);
+            data.should.have.property("longitude", -122.05740356445312);
             done();
         });
     });
     it("should provide country for an IPv6", function(done){
         geoipData("2002:7679:b9af:db9b:ab51:501a:db4e:2d", function(err, data){
-            data.should.have.property("country");
-            var country = data.country;
-            country.should.have.property("country_code", "CN");
-            country.should.have.property("country_code3", "CHN");
-            country.should.have.property("country_name", "China");
-            country.should.have.property("continent_code", "AS");
+            data.should.have.property("country_code", "CN");
+            data.should.have.property("country_code3", "CHN");
+            data.should.have.property("country_name", "China");
+            data.should.have.property("continent_code", "AS");
             done();
         });
     });
     it("should provide city for an IPv6", function(done){
         geoipData("2002:7679:b9af:db9b:ab51:501a:db4e:2d", function(err, data){
-            data.should.have.property("city");
-            var city = data.city;
-            city.should.have.property("country_code3", "CHN");
-            city.should.have.property("country_name", "China");
-            city.should.have.property("continent_code", "AS");
-            city.should.have.property("region", "32");
-            city.should.have.property("city", "Chengdu");
-            city.should.have.property("latitude", 30.66670036315918);
-            city.should.have.property("longitude", 104.06670379638672);
+            data.should.have.property("country_code3", "CHN");
+            data.should.have.property("country_name", "China");
+            data.should.have.property("continent_code", "AS");
+            data.should.have.property("region", "32");
+            data.should.have.property("city", "Chengdu");
+            data.should.have.property("latitude", 30.66670036315918);
+            data.should.have.property("longitude", 104.06670379638672);
             done();
         });
     });
