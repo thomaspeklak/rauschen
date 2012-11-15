4681b976e43a55603690e224229518cc45b40874
Thomas Peklak <thomas.peklak@gmail.com>
2012-11-15 17:19:08 +0100

    rformat frontend

--- frontend/index.js
+++ frontend/index.js
@@ -1,10 +1,10 @@
-var server  = require("./server");
-var path = require('path');
+var server = require("./server");
+var path   = require('path');
 
 var domains = require("../config/domains");
 var domain_validator = require(path.join(__dirname, "lib", "domain-restrictor"))(domains);
-server.use(domain_validator);
 
+server.use(domain_validator);
 require('./lib/distributor')(server);
 
 server.listen(Number(process.argv[2]) || 3000);
