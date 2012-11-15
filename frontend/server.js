f3701fa98068aecbfbbc8bcfec5c3fb7d96a0f57
Thomas Peklak <thomas.peklak@gmail.com>
2012-11-15 17:20:13 +0100

    rformat frontend/server

--- frontend/server.js
+++ frontend/server.js
@@ -1,12 +1,11 @@
 var express = require("express");
 app = express();
 
-app.use(function(err, req, res, next){
+app.use(function(err, req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     next();
 });
 app.use(express.bodyParser());
-
 
 require("./routes")(app);
 
