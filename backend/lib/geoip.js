7b6d168d00dddeb20f47d64ca631554dac2c002c
Thomas Peklak <thomas.peklak@gmail.com>
2012-11-15 07:03:50 +0100

    restructure geoip

--- backend/lib/geoip.js
+++ backend/lib/geoip.js
@@ -1,15 +1,9 @@
-var geoip       = require("geoip");
-var city_v4     = new geoip.City(process.env.GEOIP_CITY_DAT);
-var city_v6     = new geoip.City6(process.env.GEOIP_CITY_DAT_V6);
+var geoip   = require("geoip");
+var city_v4 = new geoip.City(process.env.GEOIP_CITY_DAT);
+var city_v6 = new geoip.City6(process.env.GEOIP_CITY_DAT_V6);
 
 module.exports = function(ip, cb){
-    var city, country;
-
-    if(ip.indexOf(":") >= 0){
-        city = city_v6;
-    } else {
-        city = city_v4;
-    }
+    var city = (ip.indexOf(":") >= 0) ? city_v6 : city_v4;
 
     city.lookup(ip, cb);
 };
