a8340e144d6333b0ef322b22544fc3d46ca80055
Thomas Peklak <thomas.peklak@gmail.com>
2012-11-16 07:22:23 +0100

    reformat code

--- frontend/index.js
+++ frontend/index.js
@@ -1,10 +1,10 @@
 var server = require("./server");
 var path   = require('path');
 
-var domains = require("../config/domains");
-var domain_validator = require(path.join(__dirname, "lib", "domain-restrictor"))(domains);
+var domains           = require("../config/domains");
+var domain_restrictor = require(path.join(__dirname, "lib", "domain-restrictor"))(domains);
 
-server.use(domain_validator);
+server.use(domain_restrictor);
 require('./lib/distributor')(server);
 
 server.listen(Number(process.argv[2]) || 3000);
