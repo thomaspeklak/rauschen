7b1ea5e05c248e68855cadea857c3fce7b057e29
Thomas Peklak <thomas.peklak@gmail.com>
2012-11-16 07:35:46 +0100

    reformat code

--- frontend/index.js
+++ frontend/index.js
@@ -5,6 +5,7 @@ var domains           = require("../config/domains");
 var domain_restrictor = require(path.join(__dirname, "lib", "domain-restrictor"))(domains);
 
 server.use(domain_restrictor);
+
 require('./lib/distributor')(server);
 
 server.listen(Number(process.argv[2]) || 3000);
