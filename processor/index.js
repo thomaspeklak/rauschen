83c24524bba73242c93043a1df23833b495b2f07
Thomas Peklak <thomas.peklak@gmail.com>
2012-12-07 16:28:26 +0100

    respaen child process if it exits not normally

--- processor/index.js
+++ processor/index.js
@@ -13,18 +13,28 @@ var timingStream = timing.createStream();
 timingStream.pipe(net.connect(socket)).pipe(timingStream);
 
 var enrichedStream = timingStream
-                        .pipe(dataExtractionStream)
-                        .pipe(dataEnrichStream)
-                        .pipe(persistenceStream);
+.pipe(dataExtractionStream)
+.pipe(dataEnrichStream)
+.pipe(persistenceStream);
 
 //enrichedStream.pipe(process.stdout);
 //timingStream.pipe(process.stdout);
 
-var spawn = require('child_process').spawn;
-var args = [  __dirname + '/../real-time-analytics' ];
-var rta = spawn(process.execPath, args, { stdio: ['pipe', 1, 2, 'ipc'] });
-enrichedStream.pipe(rta.stdin);
+function spawnRTA(){
+    var spawn = require('child_process').spawn;
+    var args = [  __dirname + '/../real-time-analytics' ];
+    var rta = spawn(process.execPath, args, { stdio: ['pipe', 1, 2, 'ipc'] });
+    enrichedStream.pipe(rta.stdin);
 
-process.on("exit", function(){
-    rta.kill();
-});
+    rta.on('exit', function(code, signal){
+        if(code === null){
+            spawnRTA();
+        }
+    });
+    process.on("exit", function(){
+        rta.kill();
+    });
+}
+
+spawnRTA();
+
