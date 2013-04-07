e82808c7eb40481dfa5fab1698c929a4cbd589fb
Thomas Peklak <thomas.peklak@gmail.com>
2013-04-07 21:50:07 +0200

    fix formatting

--- analyser/app.js
+++ analyser/app.js
@@ -5,28 +5,35 @@ var express = require("express");
 var http = require("http");
 var config = require("../config");
 var mongoskin = require("mongoskin");
-var MongoStore = require('connect-mongo')(express);
+var MongoStore = require("connect-mongo")(express);
 
 var app = express();
 
-app.db = mongoskin.db(config.db.url + "?auto_reconnect=true", {safe: false});
+app.db = mongoskin.db(config.db.url + "?auto_reconnect=true", {
+    safe: false
+});
 
 // Express settings
 app.disable("x-powered-by");
 
 // Configuration
-app.configure("development", function(){
-    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+app.configure("development", function () {
+    app.use(express.errorHandler({
+        dumpExceptions: true,
+        showStack: true
+    }));
 });
 
-app.configure("production", function(){
+app.configure("production", function () {
     app.use(express.errorHandler());
 });
 
-app.configure(function(){
+app.configure(function () {
     app.set("views", __dirname + "/views");
     app.set("view engine", "jade");
-    app.set("view options", { layout: false });
+    app.set("view options", {
+        layout: false
+    });
 
     app.use(express.logger());
     app.use(express.bodyParser());
@@ -58,7 +65,7 @@ var invokedAsMain = require.main === module;
 
 // Start server if not invoked by require("./app")
 if (invokedAsMain) {
-    http.createServer(app).listen(config.port, config.address, function() {
+    http.createServer(app).listen(config.port, config.address, function () {
         console.log("Express server listening on %s:%d in %s mode", config.address, config.port, app.settings.env);
     });
 } else {
