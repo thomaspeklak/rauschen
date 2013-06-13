c02476a78601a655f146b9f7dba52df98246a16b
Thomas Peklak <thomas.peklak@gmail.com>
2013-06-13 07:23:36 +0200

    move subprocesses in seperate node modules

--- analyser/lib/database.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var config = require("../../config");
-var mongoskin = require("mongoskin");
-
-module.exports = mongoskin.db(config.db.url + "?auto_reconnect=true", {
-    safe: false
-});
-
--- processor/lib/user-agent-detector.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var useragent = require("useragent");
-
-module.exports = function (UA, cb) {
-    process.nextTick(function () {
-        cb(null,  useragent.parse(UA));
-    });
-};
